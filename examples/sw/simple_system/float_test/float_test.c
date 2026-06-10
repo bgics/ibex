a617cc4161f11e3eadc7fe92f9fadef6fbb56048
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2026-06-10 12:36:43 +0530

    float test with pi code

--- examples/sw/simple_system/float_test/float_test.c
+++ examples/sw/simple_system/float_test/float_test.c
@@ -1,16 +1,23 @@
 #include "simple_system_common.h"
 
+volatile float result;
+
 int main(int argc, char **argv) {
   pcount_enable(0);
   pcount_reset();
   pcount_enable(1);
 
-  volatile float a = 1.5f;
-  volatile float b = 2.0f;
+  const int N = 10000;
+  const float dx = 1.0f / N;
 
-  float c = a + b;
-  float d = a * b;
-  float e = d / c;
+  float sum = 0.0f;
+
+  for (int i = 0; i < N; i++) {
+    float x = (i + 0.5f) * dx;
+    sum += 4.0f / (1.0f + x * x);
+  }
+
+  result = sum * dx;
 
   pcount_enable(0);
 
@@ -19,7 +26,7 @@ int main(int argc, char **argv) {
     uint32_t u;
   } x;
 
-  x.f = e;
+  x.f = result;
 
   puts("ANSWER: ");
   puthex(x.u);
